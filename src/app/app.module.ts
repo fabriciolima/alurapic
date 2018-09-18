a3ae113ced5f1cf35fc7b2d47c652acc4f117b85
Fabricio <fabricio.lima@gmail.com>
2018-09-18 14:06:34 -0300

    2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,20 +5,22 @@ import { AppComponent } from './app.component';
 import { FotoComponent } from './components/foto/foto.component';
 import { HttpClientModule } from '@angular/common/http';
 
-import { CardComponent } from './components/card/card.component';
+import { CardComponent } from '../bootstrap/components/card/card.component';
 import { TresPontinhosPipe } from './pipes/tres-pontinhos.pipe';
 import { CardFotoComponent } from './components/card-foto/card-foto.component';
+
+import {BootstrapModule} from '../bootstrap/bootstrap.module'
 @NgModule({
   declarations: [
     AppComponent,
     FotoComponent,
-    CardComponent,
     TresPontinhosPipe,
     CardFotoComponent
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    BootstrapModule
   ],
   providers: [],
   bootstrap: [AppComponent]
